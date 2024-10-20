d96110c2cb7951ca7e5814cdc65eb3918778bd4d
Kabesa-abraham <yebulaabraham@gmail.com>
2024-10-20 10:41:37 +0100

    Amélioration du Home
    pour le home j'ai utilisé swiper qui est un bibliothèque pour la
    création des carroussel ect... que j'ai implémenter dans mon application

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+
 import Header from "./Component/headers/Header";
 import Home from "./Component/home/Home";
 import Movie from "./Component/movie/Movie";
+ //j'ai importé le fichier css de swiper pour l'utiliser dans tout mon app
+ import 'swiper/css'
+
 
 function App() {
   return (
--- src/Component/home/Home.jsx
+++ src/Component/home/Home.jsx
@@ -1,28 +1,34 @@
-import React, { useEffect, useState } from 'react'
 import './home.css'
-import {FaPlay, FaPlayCircle} from 'react-icons/fa'
+import {FaPlay} from 'react-icons/fa'
+import 'swiper/css/autoplay'
+import {Swiper,SwiperSlide,} from 'swiper/react' //POUR FAIRE DU CARROUSSEL
+import {Autoplay} from 'swiper/modules'
 import data from '../../assets/data';
 
 const Home = () => {
 
   return (
     <div className='container' >
-      <div className='slider_wrapper' >
-        {
-           data.map((item,i) =>{
-            return <div className='image_list' key={i}>
-                <img src={item.image} alt="" className='img_item' />
-
+      <Swiper spaceBetween={2} slidesPerView={1}
+              autoplay={{delay:6000,
+                        disableOnInteraction:false }}
+              speed={1200}
+              modules={[Autoplay]}
+              className='slider_wrapper'>
+          {
+            data.map((item,i) =>{
+              return <SwiperSlide key={i} className='image_list'>
+                <img src={item.image} alt="" className='img_item'/>
                 <div className='home_text'>
-                    <span className='typeText' >{item.type}</span>
-                    <h1 className='nametext' >{item.name}</h1>
-                    <button className='bg-gradient-to-r from-indigo-700 via-purple-600 to-pink-600 bookButton' >Book Now</button>
-                    <button className='playbutton' ><FaPlay className='faplay'/></button>
-                </div>   
-            </div>
-           }) 
-        }
-      </div>
+                  <span className='typeText' >{item.type}</span>
+                  <h1 className='nametext' >{item.name}</h1>
+                  <button className='bg-gradient-to-r from-indigo-700 via-purple-600 to-pink-600 bookButton' >Book Now</button>
+                  <button className='playbutton' ><FaPlay className='faplay'/></button>
+               </div>   
+              </SwiperSlide>
+            })
+          }
+      </Swiper>
     </div>
   )
 }
